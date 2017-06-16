8631173317b0dbb317deeb975275ba1d9b47320e
Laura Gerhardt <laura.gerhardt@gsa.gov>
2017-06-16 16:16:37 -0400

    migrate jwt to vcap

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,8 +1,8 @@
 /*
 
-  ___ ___       ___               _ _       _   ___ ___ 
+  ___ ___       ___               _ _       _   ___ ___
  | __/ __|  ___| _ \___ _ _ _ __ (_) |_    /_\ | _ \_ _|
- | _|\__ \ / -_)  _/ -_) '_| '  \| |  _|  / _ \|  _/| | 
+ | _|\__ \ / -_)  _/ -_) '_| '  \| |  _|  / _ \|  _/| |
  |_| |___/ \___|_| \___|_| |_|_|_|_|\__| /_/ \_\_| |___|
 
 */
@@ -15,22 +15,23 @@
 // required modules
 
 const include = require('include')(__dirname);
-const passport = require('passport');  
+const passport = require('passport');
 const Strategy = require('passport-local');
 const bcrypt = require('bcrypt-nodejs');
 
 const models = include('src/models');
 const jwt = require('jsonwebtoken');
 const uuidV4 = require('uuid/v4');
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const VCAPServices = JSON.parse(process.env.VCAP_SERVICES);
+const JWT_SECRET_KEY = VCAPServices['user-provided'][1].credentials.JWT_SECRET_KEY;
 
 //*******************************************************************
-// passport 
+// passport
 
-passport.use(new Strategy(  
+passport.use(new Strategy(
 
 	function(un, pw, done) {
-		
+
 		models.users.findOne({
 			where: {userName: un}
 		}).then(function(user) {
@@ -40,7 +41,7 @@ passport.use(new Strategy(
 						id: user.userName,
 						role: user.userRole,
 						verified: true
-					});	
+					});
 				}
 				else {
 					done(null, false);
@@ -64,7 +65,7 @@ passport.use(new Strategy(
  * @param  {Object}   res - Response object
  * @param  {Function} next - What to call after serializing user info
  */
-const serialize = function(req, res, next) {  
+const serialize = function(req, res, next) {
 
 	req.user = {
 		id: req.user.id,
@@ -79,17 +80,17 @@ const serialize = function(req, res, next) {
  * @param  {Object}   res - Response object
  * @param  {Function} next - What to call after creating JWT
  */
-const generate = function(req, res, next) {   
-	
+const generate = function(req, res, next) {
+
 	const claims = {
-		expiresIn: 120 * 60, 
+		expiresIn: 120 * 60,
 		notBefore: 0,
-		jwtid: uuidV4(), 
-		issuer: 'fs-epermit-api', 
+		jwtid: uuidV4(),
+		issuer: 'fs-epermit-api',
 		subject: 'permit applications',
 		audience: 'fs-epermit-api-intake-users'
 	};
-	
+
 	req.token = jwt.sign({
 		id: req.user.id,
 		role: req.user.role
@@ -103,7 +104,7 @@ const generate = function(req, res, next) {
  * @param  {Object}   req - Request object
  * @param  {Object}   res - Response object
  */
-const respond = function(req, res) { 
+const respond = function(req, res) {
 
 	res.status(200).json({
 		user: req.user,
