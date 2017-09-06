f050c03690a23f2151bee84c03a3ef09bf1d633f
Greg Walker <michael.walker@gsa.gov>
2017-09-06 09:24:11 -0500

    fixes #51

---
+++ dba/migrations/06-alter-applications-02.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+	up: function (queryInterface, Sequelize) {
+		return queryInterface.changeColumn('applications', 'activity_description', {
+  			type: Sequelize.STRING(512),
+				allowNull: false
+		});
+	},
+
+	down: function (queryInterface, Sequelize) {
+		return queryInterface.changeColumn('applications', 'activity_description', {
+			type: Sequelize.STRING(255),
+			allowNull: true
+		});
+	}
+};
