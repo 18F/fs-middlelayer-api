9e2c6a961134e04f45ad824cd761a0c4f9a6de49
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-05-09 14:58:55 -0400

    add snyk name as variable

--- .circleci/store-snyk.sh
+++ .circleci/store-snyk.sh
@@ -1,3 +1,5 @@
+SNYK_REPORT=$1
+
 export STORE_BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
 export AWS_ACCESS_KEY_ID=`echo "${LOG_S3}" | jq -r .access_key_id`
 export AWS_SECRET_ACCESS_KEY=`echo "${LOG_S3}" | jq -r .secret_access_key`
