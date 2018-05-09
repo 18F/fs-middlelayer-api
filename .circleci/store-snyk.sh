353a7d84478736ac60f95aca4c75f50b9b90ba5e
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-05-09 15:25:59 -0400

    change name to hourly and declare in both needed steps

--- .circleci/store-snyk.sh
+++ .circleci/store-snyk.sh
@@ -1,9 +1,8 @@
-SNYK_REPORT=$1
+export DATE_STRING=`date +"%m-%d-%Y-%H"`
+export SNYK_REPORT=snyk-${CIRCLE_PR_REPONAME}-${DATE_STRING}.csv
 
-# export STORE_BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
-# export AWS_ACCESS_KEY_ID=`echo "${LOG_S3}" | jq -r .access_key_id`
-# export AWS_SECRET_ACCESS_KEY=`echo "${LOG_S3}" | jq -r .secret_access_key`
-# export AWS_DEFAULT_REGION=`echo "${LOG_S3}" | jq -r .region`
-echo "snyk report"
-echo "${SNYK_REPORT}"
-# aws s3 cp "snyk/${SNYK_REPORT}" s3://"${STORE_BUCKET}"/"${SNYK_REPORT}"
+export STORE_BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
+export AWS_ACCESS_KEY_ID=`echo "${LOG_S3}" | jq -r .access_key_id`
+export AWS_SECRET_ACCESS_KEY=`echo "${LOG_S3}" | jq -r .secret_access_key`
+export AWS_DEFAULT_REGION=`echo "${LOG_S3}" | jq -r .region`
+aws s3 cp "snyk/${SNYK_REPORT}" s3://"${STORE_BUCKET}"/"${SNYK_REPORT}"
