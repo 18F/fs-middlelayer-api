8b6ec3be896129b6c5a8526e6a0cf034a281bb32
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-05-09 14:41:15 -0400

    update s3 circle handling

--- .circleci/store-snyk.sh
+++ .circleci/store-snyk.sh
@@ -1,5 +1,5 @@
-export BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
+export STORE_BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
 export AWS_ACCESS_KEY_ID=`echo "${LOG_S3}" | jq -r .access_key_id`
 export AWS_SECRET_ACCESS_KEY=`echo "${LOG_S3}" | jq -r .secret_access_key`
 export AWS_DEFAULT_REGION=`echo "${LOG_S3}" | jq -r .region`
-aws s3 cp snyk s3://"${LOG_S3}" --recursive
+aws s3 cp "snyk/${SNYK_REPORT}" s3://"${STORE_BUCKET}"/"${SNYK_REPORT}"
